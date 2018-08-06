bd557d6180f525f7bf73c3c1607683d142098256
Itsuki Aoyagi <mail@yagi2.com>
2018-08-06 14:32:21 +0900

    replace kana/eisu to space and use local glcdfont file

--- keyboards/helix/rev2/keymaps/yagi2/keymap.c
+++ keyboards/helix/rev2/keymaps/yagi2/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  |  '   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | Esc  | Adjust| Alt  | GUI |Lower | Space| EISU | KANA |Space | Raise| Left | Up   | Down |Right |
+   * | Esc  | Adjust| Alt  | GUI |Lower | EISU | Space| Space|KANA  | Raise| Left | Up   | Down |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT, \
-      KC_ESC,  ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  EISU,    KANA,    KC_SPC,  RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
+      KC_ESC,  ADJUST,  KC_LALT, KC_LGUI, LOWER,   EISU,    KC_SPC,  KC_SPC,  KANA,    RAISE,   KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT  \
       ),
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
